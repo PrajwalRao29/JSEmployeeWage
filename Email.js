5b028316a5e4087ffcecd147a875ee6ebf529b4a
PrajwalRao29 <raoprajrao@gmail.com>
2020-11-17 00:17:10 +0530

    Added functionality to check optional TLD

--- Email.js
+++ Email.js
@@ -8,7 +8,7 @@ class Email{
         return this._mail;
     }
     set mail(mail){
-        let partOneRegex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.][a-z]+$')
+        let partOneRegex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.]([a-z]+){2,}([.]?[a-z]{2})?$')
         if(partOneRegex.test(mail)){
             this._mail = mail;
         }
@@ -20,10 +20,14 @@ class Email{
 try{
     let email1 = new Email("abhishek@gmail1.com")
     console.log(email1)
-    let email2 = new Email("abhishek.111@gmail.com")
+    let email2 = new Email("abhishek.111@gmail.com.co")
     console.log(email2)
-    let email3 = new Email("Abhishek.@gmail.")
-    console.log(email3)
+    // let email3 = new Email("Abhishek.@gmail.")
+    // console.log(email3)
+    // let email4 = new Email("abhishek.111@gmail.com.com")
+    // console.log(email4)
+    let email5 = new Email("abc@gmail.com.aa.au")
+    console.log(email5)
 }catch(e){
     console.log("Error :  " + e)
 }
